61cfc72a1e6308be0bd2f4cf672c1dbd4f33adad
Luis Felipe da Silva Sousa <luisfelipebcc@gmail.com>
2024-10-14 03:42:16 -0300

    Ajuste

--- src/BlogApp.Domain/Entities/User.cs
+++ src/BlogApp.Domain/Entities/User.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.AspNetCore.Identity;
-using System.Collections.Generic;
-
-namespace BlogApp.Domain.Entities
+﻿namespace BlogApp.Domain.Entities
 {
     public class User : IdentityUser
     {
